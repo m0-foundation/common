9304cdb9d3e2ab023558ac396833df9d8742ef4b
Pierrick Turelier <pierrick@turelier.com>
2023-12-05 19:41:24 -0600

    chore(github-actions): improve workflows (#4)

--- test/ContractHelper.t.sol
+++ test/ContractHelper.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.23;
 
 import { Test } from "../lib/forge-std/src/Test.sol";
 
-import { ContractHelper } from "../src/ContractHelper.sol";
+import { ContractHelperHarness } from "./utils/ContractHelperHarness.sol";
 
 contract Void {}
 
@@ -15,50 +15,52 @@ contract VoidDeployer {
 }
 
 contract ContractHelperTests is Test {
+    ContractHelperHarness internal _contractHelper;
     VoidDeployer internal _voidDeployer;
 
     function setUp() external {
+        _contractHelper = new ContractHelperHarness();
         _voidDeployer = new VoidDeployer();
     }
 
     function test_full() external {
         vm.setNonce(address(_voidDeployer), 0x00 + 1);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0x00 + 1), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0x00 + 1), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0x7f - 1);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0x7f - 1), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0x7f - 1), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0x7f);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0x7f), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0x7f), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0x7f + 1);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0x7f + 1), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0x7f + 1), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0xff - 1);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0xff - 1), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0xff - 1), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0xff);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0xff), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0xff), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0xff + 1);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0xff + 1), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0xff + 1), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0xffff - 1);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0xffff - 1), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0xffff - 1), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0xffff);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0xffff), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0xffff), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0xffff + 1);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0xffff + 1), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0xffff + 1), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0xffffff - 1);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0xffffff - 1), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0xffffff - 1), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0xffffff);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0xffffff), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0xffffff), _voidDeployer.deploy());
 
         vm.setNonce(address(_voidDeployer), 0xffffff + 1);
-        assertEq(ContractHelper.getContractFrom(address(_voidDeployer), 0xffffff + 1), _voidDeployer.deploy());
+        assertEq(_contractHelper.getContractFrom(address(_voidDeployer), 0xffffff + 1), _voidDeployer.deploy());
     }
 }
